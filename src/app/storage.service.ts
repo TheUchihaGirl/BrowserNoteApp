a35660e6135f4037af675cbe9c1326aeb7c35acc
tchowdhury <tulika.chowdhury@hyland.com>
2023-05-04 13:20:22 +0530

    Updated storage service to mitigate localstorage perkey character constraint

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -5,32 +5,78 @@ import { CardData } from 'src/model/CardData';
   providedIn: 'root'
 })
 export class StorageService {
-  private key = "datalist";
+  private key = "CardIdList";
   constructor() { }
-  set(data: any): void {
+
+  Create(cardDetails: CardData): void {
     try {
-      localStorage.setItem(this.key, JSON.stringify(data));
+      let cardIdList : string[] = this.getCardIdList();
+      let cardId : string= cardDetails.id;
+      delete cardDetails.id;
+      cardIdList.push(cardId);
+
+      localStorage.setItem(cardId,JSON.stringify(cardDetails));
+      this.saveCardIdList(cardIdList);    
     } catch (e) {
-      console.error('Error saving to localStorage', e);
+      console.error('Error saving note to localStorage', e);
     }
   }
 
-  get():CardData[] {
-    try {
-      let dataString : string | null = localStorage.getItem(this.key);
-      if(dataString == null){
-        return [];
-      }else{
-        return JSON.parse(dataString);
-      }
-    } catch (e) {
-      console.error('Error getting data from localStorage', e);
+  Update(noteDetails : CardData):void{
+    try{
+      let noteId : string = noteDetails.id;
+      localStorage.removeItem(noteId);
+      delete noteDetails.id;
+      localStorage.setItem(noteId, JSON.stringify(noteDetails));
+    }catch(e){
+      console.error('Error updating note in localstorage',e );
+    }
+  }
+
+  getAll():CardData[]{
+    try{
+      let cardIdList:string[] = this.getCardIdList();
+      let cardDetailsList : CardData[] = [];
+
+      cardIdList.forEach((cardId:string)=>{
+        let cardDetails : CardData = JSON.parse(localStorage.getItem(cardId));
+        cardDetailsList.push({
+          id:cardId,
+          ... cardDetails
+        });
+      });
+      return cardDetailsList;
+    }catch(e){
+      console.log("Error getting card details from local storage", e);
+    }
+  }
+
+  getById(id:string): CardData{
+    try{
+      let cardDetailsString = localStorage.getItem(id);
+      return JSON.parse(cardDetailsString);
+    }catch(e){
+      console.error('Error getting card details for id ',e);
+    }
+  }
+
+  private getCardIdList():string[]{
+    try{
+      let noteIdListString : string = localStorage.getItem(this.key);
+      if(noteIdListString == null) return [];
+      else return JSON.parse(noteIdListString);
+    }catch(e){
+      console.error('Error getting noteIdList from localStorage', e);
       return [];
     }
   }
 
-  getById(id:string){
-
+  private saveCardIdList(noteIdList:string[]):void{
+    try{
+      localStorage.setItem(this.key,JSON.stringify(noteIdList));
+    }catch(e){
+      console.error('Error saving noteIdList to localStorage', e);
+    }
   }
 
 
